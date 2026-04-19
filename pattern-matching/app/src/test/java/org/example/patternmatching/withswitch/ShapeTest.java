21e93554871951b3f1dda0769487cc69e65e7b69
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-19 11:59:47 -0300

    Test pattern matching with switch expression POC

---
+++ pattern-matching/app/src/test/java/org/example/patternmatching/withswitch/ShapeTest.java
@@ -0,0 +1,13 @@
+package org.example.patternmatching.withswitch;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+
+class ShapeTest {
+    @Test void patternMatchingWithSwitchWorks() {
+        final Shape r = new Rectangle(10.0, 5.0);
+        final Shape c = new Circle(2.0);
+        Stream.of(r, c).map(Shape::getPerimeter).forEach(IO::println);
+    }
+}
