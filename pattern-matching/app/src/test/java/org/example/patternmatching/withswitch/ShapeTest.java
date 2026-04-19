c451975005feafd451409ec188e2ff54eb155e03
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-19 12:06:23 -0300

    Test pattern matching with switch statement POC

--- pattern-matching/app/src/test/java/org/example/patternmatching/withswitch/ShapeTest.java
+++ pattern-matching/app/src/test/java/org/example/patternmatching/withswitch/ShapeTest.java
@@ -8,6 +8,7 @@ class ShapeTest {
     @Test void patternMatchingWithSwitchWorks() {
         final Shape r = new Rectangle(10.0, 5.0);
         final Shape c = new Circle(2.0);
-        Stream.of(r, c).map(Shape::getPerimeter).forEach(IO::println);
+        Stream.of(r, c).map(Shape::getPerimeterUsingExpression).forEach(IO::println);
+        Stream.of(r, c).map(Shape::getPerimeterUsingStatement).forEach(IO::println);
     }
 }
