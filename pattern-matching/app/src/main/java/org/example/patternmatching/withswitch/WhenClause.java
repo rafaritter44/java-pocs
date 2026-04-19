a944c0fd599fceae003b3dd148f51fe41b487052
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-19 12:30:23 -0300

    Implement when clause with record POC

--- pattern-matching/app/src/main/java/org/example/patternmatching/withswitch/WhenClause.java
+++ pattern-matching/app/src/main/java/org/example/patternmatching/withswitch/WhenClause.java
@@ -3,9 +3,17 @@ package org.example.patternmatching.withswitch;
 class WhenClause {
     static void test(final Object obj) {
         switch (obj) {
-            case String s when s.length() == 1 -> IO.println("Short: " + s);
-            case String s                      -> IO.println(s);
-            default                            -> IO.println("Not a string");
+            case final String s when s.length() == 1 -> IO.println("Short: " + s);
+            case final String s                      -> IO.println(s);
+            default                                  -> IO.println("Not a string");
+        }
+    }
+
+    record R(int x) {}
+    static void testSwitch(final R r) {
+        switch(r) {
+            case R(int x) when x >= 5 -> IO.println(x + " => 5");
+            default -> IO.println(r.x + " < 5");
         }
     }
 }
