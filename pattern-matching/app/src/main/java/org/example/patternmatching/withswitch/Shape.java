00bb267fd64aee0830940ee3bd63f6b3a54eaecb
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-19 12:06:14 -0300

    Implement pattern matching with switch statement POC

--- pattern-matching/app/src/main/java/org/example/patternmatching/withswitch/Shape.java
+++ pattern-matching/app/src/main/java/org/example/patternmatching/withswitch/Shape.java
@@ -1,11 +1,18 @@
 package org.example.patternmatching.withswitch;
 
 interface Shape {
-    static double getPerimeter(Shape s) throws IllegalArgumentException {
+    static double getPerimeterUsingExpression(final Shape s) throws IllegalArgumentException {
         return switch (s) {
-            case Rectangle r -> 2 * r.length() + 2 * r.width();
-            case Circle c -> 2 * c.radius() * Math.PI;
+            case final Rectangle r -> 2 * r.length() + 2 * r.width();
+            case final Circle c -> 2 * c.radius() * Math.PI;
             default -> throw new IllegalArgumentException("Unrecognized shape");
         };
+    }
+    static double getPerimeterUsingStatement(Shape s) throws IllegalArgumentException {
+        switch (s) {
+            case final Rectangle r: return 2 * r.length() + 2 * r.width();
+            case final Circle c: return 2 * c.radius() * Math.PI;
+            default: throw new IllegalArgumentException("Unrecognized shape");
+        }
     }
 }
