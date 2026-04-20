a765ec35245e58e17e29fd6de087ea96637ca561
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-20 17:57:38 -0300

    Test type patterns with parameterized types POC

---
+++ pattern-matching/app/src/test/java/org/example/patternmatching/typepatterns/ParameterizedTypesTest.java
@@ -0,0 +1,9 @@
+package org.example.patternmatching.typepatterns;
+
+import org.junit.jupiter.api.Test;
+
+class ParameterizedTypesTest {
+    @Test void typePatternsWithParameterizedTypesWork() {
+        ParameterizedTypes.test();
+    }
+}
