8aae18018b16880f9e017cb3b7bc8959878d770a
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-20 17:24:27 -0300

    Fix print

--- pattern-matching/app/src/main/java/org/example/patternmatching/typepatterns/ReferenceTypes.java
+++ pattern-matching/app/src/main/java/org/example/patternmatching/typepatterns/ReferenceTypes.java
@@ -1,5 +1,7 @@
 package org.example.patternmatching.typepatterns;
 
+import java.util.Arrays;
+
 class ReferenceTypes {
     static record Point(int x, int y) {}
     static enum Color { RED, GREEN, BLUE; }
@@ -10,7 +12,7 @@ class ReferenceTypes {
             case String s -> IO.println("String: %s".formatted(s));
             case Color c  -> IO.println("Color: %s".formatted(c));
             case Point p  -> IO.println("Point: %s".formatted(p));
-            case int[] ia -> IO.println("Int array: %s".formatted(ia));
+            case int[] ia -> IO.println("Int array: %s".formatted(Arrays.toString(ia)));
             default       -> IO.println("Something else");
         }
     }
