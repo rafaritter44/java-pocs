3e35f9050bc491f7f4b3334662c353baf6a8595d
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-21 12:43:04 -0300

    Implement generic record pattern vars POC

--- pattern-matching/app/src/main/java/org/example/patternmatching/recordpatterns/RecordPatternVars.java
+++ pattern-matching/app/src/main/java/org/example/patternmatching/recordpatterns/RecordPatternVars.java
@@ -2,10 +2,16 @@ package org.example.patternmatching.recordpatterns;
 
 class RecordPatternVars {
     static record Point(double x, double y) {}
-
     static void printAngleFromXAxis(final Object obj) {
         if (obj instanceof Point(final var x, final var y)) {
             IO.println(Math.toDegrees(Math.atan2(y, x)));
+        }
+    }
+
+    static record Box<T>(T t) {}
+    static void printBoxContents(final Box<String> b) {
+        if (b instanceof Box(final var s)) {
+            IO.println("Box contains: " + s);
         }
     }
 }
