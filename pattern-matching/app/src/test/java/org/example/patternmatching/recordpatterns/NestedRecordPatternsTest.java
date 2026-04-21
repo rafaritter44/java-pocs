259ffa786b3b30a04dd67a84631a151e4aa72c7f
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-21 13:08:12 -0300

    Test nested record patterns POC

---
+++ pattern-matching/app/src/test/java/org/example/patternmatching/recordpatterns/NestedRecordPatternsTest.java
@@ -0,0 +1,14 @@
+package org.example.patternmatching.recordpatterns;
+
+import org.junit.jupiter.api.Test;
+
+import static org.example.patternmatching.recordpatterns.NestedRecordPatterns.*;
+
+class NestedRecordPatternsTest {
+    @Test void nestedRecordPatternsWork() {
+        var upperLeft = new ColoredPoint(new Point(1.0, 4.0), Color.RED);
+        var lowerRight = new ColoredPoint(new Point(3.0, 1.0), Color.GREEN);
+        var r = new ColoredRectangle(upperLeft, lowerRight);
+        printXCoordOfUpperLeftPointWithPatterns(r);
+    }
+}
