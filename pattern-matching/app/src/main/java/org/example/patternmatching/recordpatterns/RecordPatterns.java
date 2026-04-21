475ec9b55d223cb3120d32f844e6683ebeb4a3e3
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-21 12:29:43 -0300

    Make fields final

--- pattern-matching/app/src/main/java/org/example/patternmatching/recordpatterns/RecordPatterns.java
+++ pattern-matching/app/src/main/java/org/example/patternmatching/recordpatterns/RecordPatterns.java
@@ -4,7 +4,7 @@ class RecordPatterns {
     static record Point(double x, double y) {}
 
     static void printAngleFromXAxis(final Object obj) {
-        if (obj instanceof Point(double x, double y)) {
+        if (obj instanceof Point(final double x, final double y)) {
             IO.println(Math.toDegrees(Math.atan2(y, x)));
         }
     }
