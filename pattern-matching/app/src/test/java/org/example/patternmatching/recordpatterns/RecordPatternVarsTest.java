1119e9b3f06633968f08de70e489f3c64992c052
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-21 12:48:02 -0300

    Test record pattern var inference POC

--- pattern-matching/app/src/test/java/org/example/patternmatching/recordpatterns/RecordPatternVarsTest.java
+++ pattern-matching/app/src/test/java/org/example/patternmatching/recordpatterns/RecordPatternVarsTest.java
@@ -6,5 +6,6 @@ class RecordPatternVarsTest {
     @Test void recordPatternVarsWork() {
         RecordPatternVars.printAngleFromXAxis(new RecordPatternVars.Point(1.0, 2.0));
         RecordPatternVars.printBoxContents(new RecordPatternVars.Box<>("str"));
+        RecordPatternVars.recordInference(new RecordPatternVars.MyPair<>("str", 3));
     }
 }
