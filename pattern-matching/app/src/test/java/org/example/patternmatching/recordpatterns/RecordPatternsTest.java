c55d65cf7c3ade403ba44882abd9bb2a00964e43
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-21 12:25:54 -0300

    Test record patterns POC

---
+++ pattern-matching/app/src/test/java/org/example/patternmatching/recordpatterns/RecordPatternsTest.java
@@ -0,0 +1,9 @@
+package org.example.patternmatching.recordpatterns;
+
+import org.junit.jupiter.api.Test;
+
+class RecordPatternsTest {
+    @Test void recordPatternsWork() {
+        RecordPatterns.printAngleFromXAxis(new RecordPatterns.Point(1.0, 2.0));
+    }
+}
